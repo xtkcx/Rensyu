1d79a55cff4ad774743286316c4455f031e9007c
xtkcx <1117take@gmail.com>
2024-06-01 05:25:33 +0900

    version03

--- src/ListSample.java
+++ src/ListSample.java
@@ -4,7 +4,25 @@ import java.util.List;
 public class ListSample {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		List<String> list = new ArrayList<String>();
+
+		//値の記憶は add メソッドを利用
+		list.add("1");
+		list.add("2");
+		list.add("3");
+		list.add("4");
+		list.add("5");
+
+		//値の取得
+		for (int i = 0; i < list.size(); i++) {
+			//値の取得はget メソッドを利用
+			System.out.println(list.get(i));
+		}
+
+		//拡張 for 文を利用するともっと簡単
+		for (String s : list) {
+			System.out.println(s);
+		}
 
 	}
 
