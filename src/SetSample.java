ebc29ab27ba5ef21a272dbb14259c0d2bd6ce67e
xtkcx <1117take@gmail.com>
2024-06-06 16:44:57 +0900

    version05

--- src/SetSample.java
+++ src/SetSample.java
@@ -5,20 +5,20 @@ public class SetSample {
 
 	public static void main(String[] args) {
 		Set<String> set = new HashSet<String>( );
-		
+
 		//値のセットは add メソッドを利用
 		set.add("value1");
 		set.add("value2");
 		set.add("value3");
 		set.add("value4");
 		set.add("value5");
-		
+
 		set.add("value2"); //重複要素を add
-		
+
 		//拡張 For 文を利用
 		for (String s : set){
-		 System.out.println(s);
-		 
+			System.out.println(s);
+
 		}
 	}
 
